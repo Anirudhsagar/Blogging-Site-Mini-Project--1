791ce18ea2c31ea4889b58bf768ca0cc97407f15
Priyanka <priyankashreevastava6@gmail.com>
2022-09-05 17:52:57 +0530

    authorModel

---
+++ src/model/autherModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const authorSchema = new mongoose.Schema({
+    fname:{
+        type:String,
+        required:true
+    },
+    lname:{
+        type:String,
+        required:true
+    },
+    title:{
+        type:String,
+        enum:['Mr','Mrs','Miss']
+    },
+    email:{
+        type:String,
+        trim:true,
+        lowercase:true,
+        required:true,
+        unique:true,
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password:{
+        type:String,
+        required:true
+
+    }
+
+
+} ,{ timestamps: true })
+module.exports = mongoose.model('Author',authorSchema)
+// { fname: { mandatory}, lname: {mandatory}, title: {mandatory, enum[Mr, Mrs, Miss]}, 
+// email: {mandatory, valid email, unique}, password: {mandatory} }
